4187dbfa9a87ce9b18d072c06d8b361dbaf09911
SajariS <santeri.sajari@gmail.com>
2026-01-28 19:16:58 +0200

    Split button lisätty sorttausta varten, logiikka käsittelyyn lisätty

--- src/components/ItemSourceList.tsx
+++ src/components/ItemSourceList.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
-import { Box, TextField } from "@mui/material";
-import type { FoodItem } from "../services/sortList";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Box, Button, ButtonGroup, ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper, TextField } from "@mui/material";
+import { sortList, type FoodItem, type FoodItemKey } from "../services/sortList";
 import { useDroppable } from "@dnd-kit/core";
 import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import ItemCard from "./ItemCard";
+import { LangContext } from "../context/LangContext";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
 
 type ItemSourceListProps = {
     sourceList: FoodItem[]
@@ -14,8 +16,66 @@ type ItemSourceListProps = {
     listRef: React.RefObject<HTMLDivElement | null>
 }
 
+type optionsRecord = {
+    i18n: string | undefined,
+    key: FoodItemKey
+}
+
 export default function ItemSourceList({sourceList, setSourceList, malleableList, setMalleableList, listId, listRef}: ItemSourceListProps) {
     const [search, setSearch] = useState<string>("")
+    const [sortOpen, setSortOpen] = useState<boolean>(false)
+    const anchorRef = useRef<HTMLDivElement>(null)
+    const [sortIndex, setSortIndex] = useState<number>(1)
+    const { texts } = useContext(LangContext)
+    const t = (key: string) => texts?.[key ?? key]
+    // Kuvaa nykyistä tilaa, sort funktio kääntää true = norm, false = inv
+    // Eli Jos tila on norm, muuttuja false = seuraava sort on inv ja toggle -> true jne.
+    const [sortInversion, setSortInversion] = useState<boolean>(false)
+
+    const sortOptions: optionsRecord[] = [
+        {i18n: t("sortOptions.name"), key: "name"}, 
+        {i18n: t("sortOptions.kcal"), key: "kcal"}, 
+        {i18n: t("sortOptions.sugar"), key: "sugar"}
+    ]
+
+    const handleSortToggle = () => {
+        setSortOpen((prevState) => !prevState)
+    }
+
+    const handleSortEvent = (inversion?: boolean, index?: number ) => {
+        // Valinnaiset param mukana koska tilamuuttja ei pysy perässä,
+        // tapahtumassa jossa sort valitaan dropdown menusta
+        // Kulku: Normaali - paina nappia -> sort + inversio toggle, uusi painallus -> inversio sort + inversio toggle
+        // Suoraan menusta: - valtise sort -> sort + inversio toggle, uusin painallaus sen jälkeen kun valittu -> inversio sort + toggle,
+        // uusi sort valinta menusta -> pakotettu normi sort + pakotettu tila inversioon
+        const sortedList = sortList(
+            malleableList, 
+            sortOptions[index === undefined ? sortIndex : index].key, 
+            inversion === undefined ? sortInversion : inversion
+        ) as FoodItem[]
+        setMalleableList(sortedList) 
+
+        if (inversion === undefined) {
+            setSortInversion((prev) => !prev)
+        }
+        else {
+            setSortInversion(!inversion)
+        }
+    }
+
+    const handleSortMenuClick = (index: number) => {
+        setSortIndex(index)
+        setSortOpen(false)
+        handleSortEvent(
+            index === sortIndex ? false : true,
+            index
+        )
+    }
+
+    const handleClose = (e: Event) => {
+        if (anchorRef.current && anchorRef.current.contains(e.target as HTMLElement)) return
+        setSortOpen(false)
+    }
 
     const { setNodeRef } = useDroppable({
         id: 'listId',
@@ -45,6 +105,8 @@ export default function ItemSourceList({sourceList, setSourceList, malleableList
         setMalleableList(filtered)
     }, [search])
 
+    if (!texts) return <p>Loading...</p>
+
     return (
         <Box>
             <TextField 
@@ -52,6 +114,51 @@ export default function ItemSourceList({sourceList, setSourceList, malleableList
                 onChange={handleChange}
                 placeholder="TODO! Hae nimen perusteella"
             />
+            <ButtonGroup
+                variant="contained"
+                ref={anchorRef}
+            >
+                <Button onClick={() => handleSortEvent()}>{sortOptions[sortIndex].i18n}</Button>
+                <Button
+                    size="small"
+                    onClick={handleSortToggle}
+                >
+                    <ArrowDropDownIcon />
+                </Button>
+            </ButtonGroup>
+            <Popper
+                sx={{ zIndex: 1}}
+                open={sortOpen}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                transition
+                disablePortal
+            >
+                {({ TransitionProps, placement}) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin: placement === "bottom" ? "center top" : "center bottom"
+                        }}
+                    >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList id="split-button-menu" autoFocusItem>
+                                    {sortOptions.map((option, index) => (
+                                        <MenuItem
+                                            key={index}
+                                            selected={index === sortIndex}
+                                            onClick={() => handleSortMenuClick(index)}
+                                        >
+                                            {option.i18n}
+                                        </MenuItem>
+                                    ))}
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
             <Box ref={setRefs}>
                 <SortableContext items={malleableList} strategy={verticalListSortingStrategy}>
                     {malleableList.map(item => (
